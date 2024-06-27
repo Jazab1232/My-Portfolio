41dc980c4d233a22eb1a0e0d52d2ef865dd0927c
Jazab Ahmad <jazabshoukat72@gmail.com>
2024-06-27 11:16:02 -0700

    update Project card

--- src/components/ProjectCard2.jsx
+++ src/components/ProjectCard2.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../styles/projects.css'
 
-export default function ProjectCard2({ title, img, desc }) {
+export default function ProjectCard2({ title, img, desc,link }) {
     return (
 
         <div className="card2">
             <div className="imgBox2">
-                <a href="/"><img src={img} alt="" /></a>
+                <a href={link} target='_blank' ><img src={img} alt="" /></a>
                 {/* <div className="overlay"></div> */}
             </div>
             <h3>{title}</h3>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ProjectCard from './ProjectCard'
+// import ProjectCard from './ProjectCard'
 import ProjectCard2 from './ProjectCard2'
 import project1 from '../assets/project1.png'
 import project2 from '../assets/project2.png'
 import project3 from '../assets/project3.png'
 import project4 from '../assets/project4.png'
 import project5 from '../assets/project5.png'
+import project6 from '../assets/project6.png'
 import '../styles/projects.css'
 
 
@@ -17,12 +18,12 @@ export default function Projects() {
                 <h1>RECENT PROJECT </h1>
             </div>
             <div className="ProjectCard">
-                <ProjectCard2 title='Countries API' desc='A responsive Web Application to fetch Countries data from an API' img={project2} />
-                <ProjectCard2 title='Todo App' desc='A responsive Web Application to create multiple todo task ' img={project4} />
-                <ProjectCard2 title='Weather App' desc='A responsive Web Application for weather forcast' img={project5} />
-                <ProjectCard2 title='Note Taking App' desc='A responsive Web Application to create and delete Notes ' img={project2} />
-                <ProjectCard2 title='Weather App' desc='A responsive Web Application for weather forcast' img={project5} />
-                <ProjectCard2 title='Todo App' desc='A responsive Web Application to create multiple todo task ' img={project4} />
+                <ProjectCard2 title='Countries API' desc='A responsive Web Application to fetch Countries data from an API' img={project1} link='https://countries-api-portfolio.netlify.app/' />
+                <ProjectCard2 title='Password Generator' desc='A responsive Web Application to create Random Password ' img={project2} link='https://password-generator-portfolio.netlify.app/ ' />
+                <ProjectCard2 title='Weather App' desc='A responsive Web Application to create Notes' img={project3} link='https://note-taking-app-portfolio.netlify.app/  ' />
+                <ProjectCard2 title='Todo App' desc='A responsive Web Application to create Todos ' img={project4} link='https://todo-app-portfolio-project.netlify.app/' />
+                <ProjectCard2 title='Weather App' desc='A responsive Web Application for weather forcast' img={project5} link='https://dashing-sunburst-fe0896.netlify.app/' />
+                <ProjectCard2 title='Simon Says Game' desc='A Web Game known as Simon Says ' img={project6} link='https://simon-says-app-portfolio.netlify.app/' />
             </div>
 
 
